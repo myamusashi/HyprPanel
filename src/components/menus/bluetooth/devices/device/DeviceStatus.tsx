2adcd4efd3b8216d94e55eb2ccc88c4df95a0aab
myamusashi <jinxprogilang666@gmail.com>
2024-12-26 11:16:02 +0700

    feat: show bluetooth battery

--- src/components/menus/bluetooth/devices/device/DeviceStatus.tsx
+++ src/components/menus/bluetooth/devices/device/DeviceStatus.tsx
@@ -1,8 +1,10 @@
-import { bind, Variable } from 'astal';
+import { bind, exec, Variable } from 'astal';
 import { Gtk } from 'astal/gtk3';
 import AstalBluetooth from 'gi://AstalBluetooth?version=0.1';
 
 export const DeviceStatus = ({ device }: DeviceStatusProps): JSX.Element => {
+    const btBatteryBinding = exec(['bash', '-c', `bluetoothctl info | grep -v ${device.address} | grep 'Batt' | awk '{print $4}' | head -1 | tr -d '()' | awk '{print $1"%"}'`]);
+
     const revealerBinding = Variable.derive(
         [bind(device, 'connected'), bind(device, 'paired')],
         (connected, paired) => {
@@ -21,7 +23,7 @@ export const DeviceStatus = ({ device }: DeviceStatusProps): JSX.Element => {
             <label
                 halign={Gtk.Align.START}
                 className={'connection-status dim'}
-                label={bind(device, 'connected').as((connected) => (connected ? 'Connected' : 'Paired'))}
+                label={bind(device, 'connected').as((connected) => (connected ? `Connected ${btBatteryBinding}` : 'Paired'))}
             />
         </revealer>
     );
